68d7238a8414ab31517f0309c2dab0626bc31f3f
Sque1ze <andriy2005playgame@gmail.com>
2025-04-02 01:16:02 +0300

    Створив нову сторінку для покупців

---
+++ Dealship/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Dealship.Models;
+
+namespace Dealship.Controllers;
+
+public class MainController : Controller
+{
+    private readonly ILogger<MainController> _logger;
+
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
